554403d655beac4c2f818acf1c6b498d05fc3ead
hugoyin <zy1265@nyu.edu>
2023-10-12 15:19:24 -0500

    Adding Cache Annotations

--- src/main/java/com/laioffer/twitch/external/TwitchService.java
+++ src/main/java/com/laioffer/twitch/external/TwitchService.java
@@ -1,31 +1,37 @@
 package com.laioffer.twitch.external;
 
+
 import com.laioffer.twitch.external.model.Clip;
 import com.laioffer.twitch.external.model.Game;
 import com.laioffer.twitch.external.model.Stream;
 import com.laioffer.twitch.external.model.Video;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
 public class TwitchService {
 
+
     private final TwitchApiClient twitchApiClient;
+
 
     public TwitchService(TwitchApiClient twitchApiClient) {
         this.twitchApiClient = twitchApiClient;
     }
 
 
+    @Cacheable("top_games")
     public List<Game> getTopGames() {
         return twitchApiClient.getTopGames().data();
     }
 
 
-
-
+    @Cacheable("games_by_name")
     public List<Game> getGames(String name) {
         return twitchApiClient.getGames(name).data();
     }
@@ -53,5 +59,4 @@ public class TwitchService {
         }
         return topGameIds;
     }
-
 }
